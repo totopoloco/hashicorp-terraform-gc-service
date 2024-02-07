8a70778d157052494cde4f39efc63f0476c501d9
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-07 12:38:23 +0100

    none VPC Network peering

--- 02_custom_network.tf
+++ 02_custom_network.tf
@@ -1,5 +1,5 @@
-resource "google_compute_network" "vpc_network_custom" {
-  name                    = "terraform-network-custom"
+resource "google_compute_network" "vpc_network_custom_1" {
+  name = "terraform-network-custom-1"
   auto_create_subnetworks = "false"
   description             = "VPC network created by Terraform with custom subnet"
   routing_mode            = "REGIONAL"
@@ -10,7 +10,7 @@ resource "google_compute_subnetwork" "subnet_1" {
   name          = "subnet-1"
   ip_cidr_range = "10.0.1.0/24"
   region        = "europe-west6"
-  network       = google_compute_network.vpc_network_custom.self_link
+  network = google_compute_network.vpc_network_custom_1.self_link
   project       = "marco-t-avila-project-3"
 }
 
@@ -18,13 +18,13 @@ resource "google_compute_subnetwork" "subnet_2" {
   name          = "subnet-2"
   ip_cidr_range = "10.0.2.0/24"
   region        = "europe-west6"
-  network       = google_compute_network.vpc_network_custom.self_link
+  network = google_compute_network.vpc_network_custom_1.self_link
   project       = "marco-t-avila-project-3"
 }
 
 resource "google_compute_firewall" "block_http_inbound_custom" {
   name    = "block-http-inbound-custom"
-  network = google_compute_network.vpc_network_custom.self_link
+  network = google_compute_network.vpc_network_custom_1.self_link
 
   deny {
     protocol = "tcp"
@@ -37,7 +37,7 @@ resource "google_compute_firewall" "block_http_inbound_custom" {
 
 resource "google_compute_firewall" "block_http_outbound_custom" {
   name    = "block-http-outbound-custom"
-  network = google_compute_network.vpc_network_custom.self_link
+  network = google_compute_network.vpc_network_custom_1.self_link
 
   deny {
     protocol = "tcp"
---
+++ 03_custom_network.tf
@@ -0,0 +1,23 @@
+resource "google_compute_network" "vpc_network_custom_2" {
+  name                    = "terraform-network-custom-2"
+  auto_create_subnetworks = "false"
+  description             = "VPC network created by Terraform with custom subnet"
+  routing_mode            = "REGIONAL"
+  project                 = "marco-t-avila-project-3"
+}
+
+resource "google_compute_subnetwork" "subnet_3" {
+  name          = "subnet-3"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "europe-west6"
+  network       = google_compute_network.vpc_network_custom_2.self_link
+  project       = "marco-t-avila-project-3"
+}
+
+resource "google_compute_subnetwork" "subnet_4" {
+  name          = "subnet-4"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "europe-west6"
+  network       = google_compute_network.vpc_network_custom_2.self_link
+  project       = "marco-t-avila-project-3"
+}
