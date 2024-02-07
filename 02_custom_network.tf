a5a220a7f3105a45500354b89f5e6d1c8ac55ebd
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-07 20:09:04 +0100

    none re enable configuration II

--- 02_custom_network.tf
+++ 02_custom_network.tf
@@ -22,28 +22,15 @@ resource "google_compute_subnetwork" "subnet_2" {
   project = var.project_id_2
 }
 
-resource "google_compute_firewall" "block_http_inbound_custom" {
-  name    = "block-http-inbound-custom"
+resource "google_compute_firewall" "allow_https_inbound_custom_1" {
+  name    = "allow-https-inbound-custom-1"
   network = google_compute_network.vpc_network_custom_1.self_link
+  project = var.project_id_2
 
-  deny {
+  allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports = ["22", "80", "443"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  direction     = "INGRESS"
-}
-
-resource "google_compute_firewall" "block_http_outbound_custom" {
-  name    = "block-http-outbound-custom"
-  network = google_compute_network.vpc_network_custom_1.self_link
-
-  deny {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  destination_ranges = ["0.0.0.0/0"]
-  direction          = "EGRESS"
 }
