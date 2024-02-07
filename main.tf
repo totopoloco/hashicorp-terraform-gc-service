85eb03c81a92c7a64bc02ecf0532ef567017fed2
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-07 11:19:55 +0100

    none Creates a custom virtual network

--- main.tf
+++ main.tf
@@ -18,9 +18,33 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
   auto_create_subnetworks = "true"
-  description = "VPC network created by Terraform"
+  description = "VPC network created by Terraform with auto subnets"
   mtu = 1460
   routing_mode = "REGIONAL"
   project = "marco-t-avila-project-3"
 
+}
+
+resource "google_compute_network" "vpc_network_custom" {
+  name                    = "terraform-network-custom"
+  auto_create_subnetworks = "false"
+  description             = "VPC network created by Terraform with custom subnet"
+  routing_mode            = "REGIONAL"
+  project                 = "marco-t-avila-project-3"
+}
+
+resource "google_compute_network" "subnet_1" {
+  name          = "subnet-1"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "europe-west6"
+  network       = google_compute_network.vpc_network_custom.self_link
+  project       = "marco-t-avila-project-3"
+}
+
+resource "google_compute_network" "subnet_2" {
+  name          = "subnet-2"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "europe-west6"
+  network       = google_compute_network.vpc_network_custom.self_link
+  project       = "marco-t-avila-project-3"
 }
