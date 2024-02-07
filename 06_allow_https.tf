9808c69f48c262f140ccf8a7c2519debb2d3a21d
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-07 18:45:04 +0100

    none Adds the firewall for https

---
+++ 06_allow_https.tf
@@ -0,0 +1,12 @@
+resource "google_compute_firewall" "allow_https_inbound_custom_1" {
+  name    = "allow-https-inbound-custom-1"
+  network = google_compute_network.vpc_network_custom_1.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  direction     = "INGRESS"
+}
