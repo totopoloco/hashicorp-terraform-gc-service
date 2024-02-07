4015447c5cc9bc32a3db51a02a0cb479dc337eee
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-07 18:24:37 +0100

    none Adds a vm

--- 06_vm_with_server.tf
+++ 06_vm_with_server.tf
@@ -5,7 +5,7 @@ resource "google_compute_instance" "vm_instance_1" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 
