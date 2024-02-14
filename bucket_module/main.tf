a197918ad5a749618394ab07b410e903fbce8deb
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-14 14:45:27 +0100

    none starting with the bucket

--- bucket_module/main.tf
+++ bucket_module/main.tf
@@ -7,24 +7,7 @@ output "client_email" {
   value = local.client_email
 }
 
-resource "null_resource" "check_iam_policy" {
-  triggers = {
-    command = <<EOF
-      EXISTS=$(gcloud projects get-iam-policy marco-t-avila-project-3 --format=json | jq -r '.bindings[] | select(.role == "roles/storage.admin") | .members[]' | grep -q "serviceAccount:terraform-command-line@marco-t-avila-project-3.iam.gserviceaccount.com" && echo "true" || echo "false")
-      if [ "$EXISTS" = "true" ]; then
-        echo "Warning: The storage role is already assigned to the client email"
-      fi
-    EOF
-  }
-
-  provisioner "local-exec" {
-    command     = self.triggers.command
-    interpreter = ["/bin/sh", "-c"]
-  }
-}
-
 resource "google_project_iam_member" "member" {
-  count   = null_resource.check_iam_policy.triggers.command == "" ? 1 : 0
   member  = "serviceAccount:${local.client_email}"
   project = var.project_id
   role    = var.role_storage
