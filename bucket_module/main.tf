f1ff691d9c68eb7c4c5d775cc09fd5eb9951e69a
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-14 14:22:45 +0100

    none starting with the bucket

--- bucket_module/main.tf
+++ bucket_module/main.tf
@@ -7,24 +7,6 @@ output "client_email" {
   value = local.client_email
 }
 
-resource "google_project_iam_policy" "project" {
-  provider = google-beta
-  project  = var.project_id
-
-  policy_data = <<EOF
-{
-  "bindings": [
-    {
-      "role": "roles/editor",
-      "members": [
-        "serviceAccount:${var.client_email}"
-      ]
-    }
-  ]
-}
-EOF
-}
-
 resource "google_project_iam_member" "member" {
   member  = "serviceAccount:${local.client_email}"
   project = var.project_id
