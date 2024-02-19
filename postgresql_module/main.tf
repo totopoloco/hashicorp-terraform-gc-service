98a9f15723c78a918bc4ed83f5d0859077b9797e
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2024-02-19 16:21:42 +0100

    none added PostgreSQL

--- postgresql_module/main.tf
+++ postgresql_module/main.tf
@@ -14,6 +14,28 @@ resource "google_project_iam_member" "member" {
 }
 # End of the role creation
 
+#Create the database networking
+resource "google_project_service" "service_networking" {
+  project                    = var.instance_project_id
+  service                    = "servicenetworking.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  name = "private-ip-address"
+  purpose = "VPC_PEERING"
+  address_type = "INTERNAL"
+  prefix_length = 16
+  network = var.instance_network
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = var.instance_network
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  service                 = "servicenetworking.googleapis.com"
+}
+#End of the database networking creation
+
 resource "google_sql_database_instance" "default" {
   database_version = var.instance_database_version
   name             = var.instance_name
